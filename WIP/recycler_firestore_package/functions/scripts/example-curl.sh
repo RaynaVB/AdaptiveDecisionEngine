b55e640aec3c651bc3cfeff3a68d90df4dce7a9b
Rayna Budigelli <raynabudigelli@gmail.com>
2026-03-26 18:20:42 -0400

    chore: Delete various WIP files including Firestore configurations, seed data, documentation, and related Cloud Functions code.

--- WIP/recycler_firestore_package/functions/scripts/example-curl.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST "$FUNCTION_URL/importTaxonomy" \
-  -H "Content-Type: application/json" \
-  -H "x-admin-key: $TAXONOMY_ADMIN_KEY" \
-  -d @../examples/import_ingredients_payload.json
--- WIP/recycler_firestore_package/functions/src/lib/firestore.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-if (!admin.apps.length) {
-  admin.initializeApp();
-}
-
-export const db = admin.firestore();
-export const FieldValue = admin.firestore.FieldValue;
